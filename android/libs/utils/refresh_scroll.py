70c639b1539acf8ba340962c5dda83f2fd882c34
Muneeb1195 <muneeb121.msm@gmail.com>
2026-06-09 00:29:03 +0500

    Pull-to-refresh on card lists
    - New RefreshableScrollView widget (overscroll detection)
    - Replaced ScrollViews in customers, inventory (3 tabs),
      expenses, staff (3 tabs), sales_history with RefreshableScrollView
    - Each wired to its respective rebuild callback

---
+++ android/libs/utils/refresh_scroll.py
@@ -0,0 +1,35 @@
+from kivy.uix.scrollview import ScrollView
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+
+
+class RefreshableScrollView(ScrollView):
+    refresh_callback = ObjectProperty(None, allownone=True)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._refreshing = False
+        self._start_y = 0
+        self.effect_cls = "ScrollEffect"
+
+    def on_touch_down(self, touch):
+        if not touch.is_mouse_scrolling:
+            self._start_y = touch.y
+        return super().on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        if not self._refreshing and self.refresh_callback and self.scroll_y > 1.05:
+            self._refreshing = True
+            Clock.schedule_once(self._do_refresh, 0.3)
+        return super().on_touch_up(touch)
+
+    def _do_refresh(self, dt):
+        try:
+            if self.refresh_callback:
+                self.refresh_callback()
+        finally:
+            Clock.schedule_once(self._reset, 0.5)
+
+    def _reset(self, dt):
+        self._refreshing = False
+        self.scroll_to(self, padding=0, animate=False)
